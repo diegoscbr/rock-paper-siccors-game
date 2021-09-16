6bd23bc8b3020971e2900317358d0694f73d182e
diegoscbr <diego.e@me.com>
2021-09-16 14:34:04 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,18 +2,41 @@
 
 import random 
 
-print("Rock, Paper, Scissors, Shoot!")
+print(" WELCOME TO Rock, Paper, Scissors, Shoot!")
 
 #propt user for input
 
 #x = input("Choose 'rock' or 'paper' or 'scissors' ")
 user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+print("User chose:")
 print(user_choice)
 
 #COMPUTER CHOICE AT RANDOM
 
-options = ["rock", "paper", "siccors"]
+options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
+print("Computer chose:")
 print(computer_choice)
 
+
+if computer_choice == user_choice:
+    print ("Draw!")
+    
+elif computer_choice == 'rock' and user_choice == 'scissors':
+    print ("Computer wins, rock beat scissors, try again!")
+
+elif computer_choice == 'scissors' and user_choice == 'paper':
+    print ("Computer wins, scissors beat paper, try again!")
+
+elif computer_choice == 'paper' and user_choice =='rock':
+    print ("Computer wins, paper beat rock, try again!")
+
+elif computer_choice == 'scissors' and user_choice == 'rock':
+    print ("YOU WIN! Guess you're smarter than a computer;)")
+
+elif computer_choice == 'paper' and user_choice == 'scissors':
+    print ("YOU WIN! Guess you're smarter than a computer;)")
+
+elif computer_choice == 'rock' and user_choice == 'paper':
+    print ("YOU WIN! Guess you're smarter than a computer;)")
